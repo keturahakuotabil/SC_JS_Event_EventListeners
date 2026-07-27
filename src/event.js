80c1e4f2fb2cbc7c1c3604790fb6bd7219817f78
ElpeAJ <elpediaarthurjunior@gmail.com>
2026-07-27 02:54:46 +0000

    Adding JS file with steps as comments

---
+++ src/event.js
@@ -0,0 +1,28 @@
+// Selecting our HTML targets
+
+
+// (i) Click Event Demo
+
+
+// (ii) Mouseover Event Demo
+
+
+// Reset style when mouse leaves (Good for student UX awareness)
+
+
+// (iii) Keydown Event Demo
+
+
+
+// TASK: CLICK EVENTS WITH IMAGE (TOGGLE DEMO)
+
+
+
+
+
+// TASK 5: INLINE HTML EVENT HANDLING ATTRIBUTES
+// These functions are called directly from the HTML 'onclick' and 'onmouseenter' attributes.
+
+
+
+
